d7086825bca2faf5858455a5f11cdb0243d52fe8
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-06-14 16:28:33 +0800

    optimised and fixed bugs for usermytasks

--- backend/controllers/userMyTasksController.js
+++ backend/controllers/userMyTasksController.js
@@ -3,6 +3,7 @@ const db = require("../util/database");
 const sequelize = require("../util/database");
 // const task = require("./../models/Task");
 // const taskJSONable = require("./../models/taskJSONable");
+const { Op } = require("sequelize");
 
 // import statements for models and object TODO
 // import models
@@ -26,56 +27,60 @@ const getMyTasksUser = async (req, res, next) => {
       error: "user_id is required for getting your tasks!",
     });
   }
-  // try {
-    // query people project for project name
-    // query people task group for task groups
-    // query people task for tasks
-    // console.log("bleh");=
+  try {
     const user = await Person.findOne({ where: { user_id: user_id } });
     if (!user) {
       return res.status(404).json({ error: "user_id not found" });
     }
-
     // idarr is the array of project ids
-    const idarr = await PersonProject.findAll({
+    const idarr = PersonProject.findAll({
       attributes: [
         [sequelize.fn("DISTINCT", sequelize.col("project_id")), "project_id"],
       ],
       where: { user_id: user_id },
     }).then((projidarr) => projidarr.map((x) => x.project_id));
-
-    //nameoutarr is the Promise array of project names
-    const nameoutarr = idarr.map((id) =>
-      Project.findOne({ where: { project_id: id } })
+    // projArr is the Promise array of project objs
+    const projArr = idarr.then(
+      async (idarr) =>
+        await Project.findAll({
+          attributes: [
+            [
+              sequelize.fn("DISTINCT", sequelize.col("project_id")),
+              "project_id",
+            ],
+          ],
+          where: { project_id: idarr },
+        })
     );
-    // array of task ids
-    const taskIds = await PersonTask.findAll({
+    // array of task id promises
+    const taskIds = PersonTask.findAll({
       attributes: [
         [sequelize.fn("DISTINCT", sequelize.col("task_id")), "task_id"],
       ],
       where: { user_id: user_id },
     });
-    // query for tasks one by one (possible optimisation?)
     // array of task promises
-    const tasks = taskIds.map(async (taskId) => Task.findOne({ where: { task_id: taskId } }));
-    console.log(tasks);
-    // array of taskgroupid promises
-    const taskGroupIds = taskIds.map(async (taskId) =>
-      TaskGroupTask.findOne({ where: { task_id: await taskId } })
+    const tasks = taskIds.then((idarr) =>
+      Task.findAll({
+        where: { task_id: idarr },
+      })
     );
-    // array of taskgroups promises
-    const taskgroup = taskGroupIds.map(async (taskGroupId) => {
-      const id = await taskGroupId;
-      return TaskGroup.findOne({ where: { group_id: id } });
-    });
-    Promise.all([
-      Promise.all(nameoutarr),
-      Promise.all(tasks),
-      Promise.all(taskgroup),
-    ]).then((fuckyou) => {
-      const projs = fuckyou[0];
-      const tasks = fuckyou[1];
-      const taskGroups = fuckyou[2];
+    // array of taskgroupid promises
+    const taskGroupIds = taskIds.then((idarr) =>
+      TaskGroupTask.findAll({
+        where: { task_id: idarr },
+      })
+    );
+    // array of taskgroup promises
+    const taskgroup = taskGroupIds.then((idarr) =>
+      TaskGroup.findAll({
+        where: { group_id: idarr },
+      })
+    );
+    return await Promise.all([projArr, tasks, taskgroup]).then((array) => {
+      const projs = array[0];
+      const tasks = array[1];
+      const taskGroups = array[2];
       const outArr = [];
       const index = 0;
       for (const task of tasks) {
@@ -96,12 +101,10 @@ const getMyTasksUser = async (req, res, next) => {
       console.log(outArr);
       return res.status(201).json({ success: "success", tasks: outArr });
     });
-
-    // query people task table to see relevant task id
-    // query project task table to see relevant project name
-  // } catch (err) {
-  //   return res.status(401).json({ error: err });
-  // }
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ error: err });
+  }
 };
 
 module.exports = { getMyTasksUser };
