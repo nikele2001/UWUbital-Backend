52134e3d88e9f0c5a93ab5a824168e36bab5313e
Nicholas <nicholaschiazhijie2001@yahoo.com>
2023-06-17 01:04:22 +0800

    optimised some controllers and removed controllers and routes with /task endpoints (for supposed future development)

--- backend/controllers/userTaskManagementController.js
+++ backend/controllers/userTaskManagementController.js
@@ -15,35 +15,35 @@ const {
   TaskGroupTask,
 } = require("./../models/relations");
 
-// not needed for now
-const PUTTaskUser = async (req, res, next) => {
-  const { project_id, group_id } = req.body;
-  if (!project_id || !group_id) {
-    return res.status(403).json({
-      error: "group ID and project ID are required for adding task to project",
-    });
-  }
-};
+// // not needed for now
+// const PUTTaskUser = async (req, res, next) => {
+//   const { project_id, group_id } = req.body;
+//   if (!project_id || !group_id) {
+//     return res.status(403).json({
+//       error: "group ID and project ID are required for adding task to project",
+//     });
+//   }
+// };
 
-// not needed for now
-const DELETETaskUser = async (req, res, next) => {
-  const { task_id } = req.body;
-  if (!task_id) {
-    return res.status(403).json({
-      error: "task ID is required for deleting task from project",
-    });
-  }
-};
+// // not needed for now
+// const DELETETaskUser = async (req, res, next) => {
+//   const { task_id } = req.body;
+//   if (!task_id) {
+//     return res.status(403).json({
+//       error: "task ID is required for deleting task from project",
+//     });
+//   }
+// };
 
-// not needed for now
-const PATCHTaskUser = async (req, res, next) => {
-  const { taskJSON } = req.body;
-  if (!taskJSON) {
-    return res.status(403).json({
-      error: "task JSON is required for editing task in project",
-    });
-  }
-};
+// // not needed for now
+// const PATCHTaskUser = async (req, res, next) => {
+//   const { taskJSON } = req.body;
+//   if (!taskJSON) {
+//     return res.status(403).json({
+//       error: "task JSON is required for editing task in project",
+//     });
+//   }
+// };
 
 const PUTTaskGroupUser = async (req, res, next) => {
   const { project_id, pax, task_group_name, task_arr_JSON } = req.body;
@@ -86,12 +86,7 @@ const PUTTaskGroupUser = async (req, res, next) => {
   Promise.all(taskJSON_promise_arr)
     // create record in task table
     .then(async (result) => {
-      console.log("adding tasks to tasks table...");
-      for (let i = 0; i < result.length; i++) {
-        result[i] = await Task.create({
-          task_JSON: JSON.stringify(result[i]),
-        });
-      }
+      await Task.bulkCreate(result.map((x) => JSON.stringify(x)));
       return result;
     })
     .then(async (result) => {
@@ -101,6 +96,7 @@ const PUTTaskGroupUser = async (req, res, next) => {
         let new_result = JSON.parse(result[i].task_JSON);
         new_result.task_id = result[i].task_id;
         new_result.group_id = group_id;
+        // populate people_taskgroup and people_task relation tables
         if (new_result.user_id !== null) {
           await PersonTaskGroup.findOrCreate({
             where: { user_id: Number(new_result.user_id), group_id: group_id },
@@ -111,7 +107,6 @@ const PUTTaskGroupUser = async (req, res, next) => {
               task_id: new_result.task_id,
             },
           });
-          console.log("updated relations table!");
         }
         new_result = JSON.stringify(new_result);
         await Task.update(
@@ -123,35 +118,25 @@ const PUTTaskGroupUser = async (req, res, next) => {
     })
     // create record in project task table
     .then(async (result) => {
-      console.log("adding records in project task table...");
-      for (let i = 0; i < result.length; i++) {
-        result[i] = await ProjectTask.create({
-          project_id: project_id,
-          task_id: result[i].task_id,
-        });
-      }
+      await ProjectTask.bulkCreate(
+        result.map((x) => new { project_id: project_id, task_id: x.task_id }())
+      );
       return result;
     })
     // create record in task group task table
     .then(async (result) => {
-      console.log("adding records into task group task table...");
-      for (let i = 0; i < result.length; i++) {
-        result[i] = await TaskGroupTask.create({
-          group_id: group_id,
-          task_id: result[i].task_id,
-        });
-      }
+      await TaskGroupTask.bulkCreate(
+        result.map((x) => new { group_id: group_id, task_id: x.task_id }())
+      );
       return result;
     })
     // add task id to idarray
     .then((result) => {
-      console.log("pushing task ids into id array...");
       for (let i = 0; i < result.length; i++) {
         id_array.push(result[i].task_id);
       }
     })
     .then(() => {
-      console.log("task group added successfully");
       return res.status(201).json({
         success: "task group and respective tasks added successfully",
         group_id: group_id,
@@ -159,7 +144,6 @@ const PUTTaskGroupUser = async (req, res, next) => {
       });
     })
     .catch((err) => {
-      console.log(err);
       return res.status(401).json({
         error: err,
       });
@@ -199,12 +183,10 @@ const PATCHTaskGroupUser = async (req, res, next) => {
   }
 
   // finding project id so that project task table can be updated accordingly
-  const proj = await ProjectTaskGroup.findAll({
+  const proj = await ProjectTaskGroup.findOne({
     where: { group_id: group_id },
   });
-  const project_id = proj[0].project_id;
-  console.log("number: " + project_id);
-  console.log("finding...");
+  const project_id = proj.project_id;
   let id_array = [];
 
   // updating pax in taskgroup table
@@ -217,34 +199,25 @@ const PATCHTaskGroupUser = async (req, res, next) => {
     ],
     where: { group_id: group_id },
   }).then(async (result) => {
-    // POSSIBLE OPTIMISATION
-    for (let i = 0; i < result.length; i++) {
-      await Task.destroy({ where: { task_id: result[i].task_id } });
-    }
+    await Task.destroy({ where: { task_id: result.map((x) => x.task_id) } });
     return result;
   });
 
   taskJSON_promise_arr = task_arr_JSON.map(
     (x) => new Promise((resolve, reject) => resolve(x))
   );
-  console.log("Removed existing tasks under task group. adding new tasks...");
 
   // adding updated tasks to task group and relevant relation tables
   Promise.all(taskJSON_promise_arr)
     // create record in task table
     .then(async (result) => {
-      console.log("adding tasks to tasks table...");
-      for (let i = 0; i < result.length; i++) {
-        // console.log(result[i]);
-        result[i] = await Task.create({
-          task_JSON: JSON.stringify(result[i]),
-        });
-      }
+      await Task.bulkCreate({
+        task_JSON: result.map((x) => JSON.stringify(x)),
+      });
       return result;
     })
     .then(async (result) => {
       // adding task IDs to task_JSON
-      console.log("adding task IDs to taskJSON...");
       for (let i = 0; i < result.length; i++) {
         let new_result = JSON.parse(result[i].task_JSON);
         new_result.task_id = result[i].task_id;
@@ -259,7 +232,6 @@ const PATCHTaskGroupUser = async (req, res, next) => {
               task_id: new_result.task_id,
             },
           });
-          console.log("updated relations table!");
         }
         new_result = JSON.stringify(new_result);
         await Task.update(
@@ -270,36 +242,25 @@ const PATCHTaskGroupUser = async (req, res, next) => {
       return result;
     })
     .then(async (result) => {
-      console.log("adding records in project task table...");
-      for (let i = 0; i < result.length; i++) {
-        result[i] = await ProjectTask.create({
-          project_id: project_id,
-          task_id: result[i].task_id,
-        });
-      }
+      await ProjectTask.bulkCreate(
+        new { project_id: project_id, task_id: result.map((x) => x.task_id) }()
+      );
       return result;
     })
     // create record in task group task table
     .then(async (result) => {
-      console.log("adding records into task group task table...");
-      for (let i = 0; i < result.length; i++) {
-        result[i] = await TaskGroupTask.create({
-          group_id: group_id,
-          task_id: result[i].task_id,
-        });
-      }
+      await TaskGroupTask.bulkCreate(
+        new { group_id: group_id, task_id: result.map((x) => x.task_id) }()
+      );
       return result;
     })
     // add task id to idarray
     .then((result) => {
-      console.log("pushing task ids into id array...");
       for (let i = 0; i < result.length; i++) {
-        // console.log(result[i].task_id);
         id_array.push(result[i].task_id);
       }
     })
     .then(() => {
-      console.log("task group added successfully");
       return res.status(201).json({
         success: "task group and respective tasks added successfully",
         id_array: id_array,
@@ -313,9 +274,9 @@ const PATCHTaskGroupUser = async (req, res, next) => {
 };
 
 module.exports = {
-  PUTTaskUser,
-  DELETETaskUser,
-  PATCHTaskUser,
+  // PUTTaskUser,
+  // DELETETaskUser,
+  // PATCHTaskUser,
   PUTTaskGroupUser,
   DELETETaskGroupUser,
   PATCHTaskGroupUser,
