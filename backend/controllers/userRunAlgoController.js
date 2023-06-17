32ff9143bbbdb37f5e6cea804324df20ade2a3ed
Nicholas <nicholaschiazhijie2001@yahoo.com>
2023-06-17 21:54:36 +0800

    updated algorithm

--- backend/controllers/userRunAlgoController.js
+++ backend/controllers/userRunAlgoController.js
@@ -12,9 +12,22 @@ const {
   ProjectTask,
   TaskGroupTask,
 } = require("./../models/relations");
+const Algo = require("./../algorithm/greedy-algo1");
 
+// return a project: projectJSONable
 const runUser = async (req, res, next) => {
-  return res.status(201).json({ success: "fk u algo not up" });
+  const { projectJSONable } = req.body;
+  if (!projectJSONable) {
+    return res
+      .status(403)
+      .json({ error: "projectJSONable is required to run algorithm!" });
+  }
+  // const project = Project.fromJSONable(projectJSONable);
+
+  return res.status(201).json({
+    success: "algorithm has completed execution",
+    projectJSONable: Algo.runGreedyAlgorithm(projectJSONable).toJSONable(),
+  });
 };
 
 module.exports = { runUser };
