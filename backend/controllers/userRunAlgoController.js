61f5ef641951458c53f64c45023655bcff045cd0
Nicholas <nicholaschiazhijie2001@yahoo.com>
2023-06-18 12:50:44 +0800

    NOT WORKING YET: updating /run controller to write assignments into database

--- backend/controllers/userRunAlgoController.js
+++ backend/controllers/userRunAlgoController.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 // import statements for models and object TODO
-const Person = require("./../algorithm/Person");
 const { Project } = require("./../algorithm/Project");
-const TaskGroup = require("./../algorithm/TaskGroup");
-const Task = require("./../algorithm/Task");
 const Algo = require("./../algorithm/greedy-algo1");
+
+const Task = require("./../models/tasks");
+const { PersonTaskGroup, PersonTask } = require("../models/relations");
 
 // return a project: projectJSONable
 const runUser = async (req, res, next) => {
@@ -14,11 +14,62 @@ const runUser = async (req, res, next) => {
       .status(403)
       .json({ error: "projectJSONable is required to run algorithm!" });
   }
-  const projectobj = Project.fromJSONable(project);
+  try {
+    proj = Project.fromJSONable(project);
+  } catch (err) {
+    return res.status(401).json({ error: "project is not JSONable" });
+  }
+
+  // a projectJSONable after running algorithm
+  const finalAssignments = Algo.runGreedyAlgorithm(proj, 3).toJSONable();
+  // all assigned tasks in the project (including preassignments)
+  const assignedTaskGroups = finalAssignments.taskGroups.map((x) => {
+    const taskarr = x.tasks;
+    x.useridarr = [];
+    taskarr.map((y) => {
+      x.useridarr.push(y.user_id);
+      return y;
+    });
+    return x;
+  });
+  console.log(assignedTaskGroups);
+
+  // update people task group table
+  for (let i = 0; i < assignedTaskGroups.length; i++) {
+    for (let j = 0; j < assignedTaskGroups[i].useridarr.length; j++) {
+      await PersonTaskGroup.findOrCreate({
+        where: {
+          user_id: assignedTaskGroups[i].useridarr[j],
+          group_id: assignedTaskGroups[i].id,
+        },
+      });
+    }
+  }
+
+  // update Task, personTask based on new information
+  const taskarr = assignedTaskGroups
+    .map((x) => x.tasks)
+    .reduce((a, b) => a.concat(b), []);
+  console.log(taskarr);
+
+  for (let i = 0; i < taskarr.length; i++) {
+    await PersonTask.findOrCreate({
+      where: {
+        user_id: taskarr[i].user_id,
+        task_id: taskarr[i].task_id,
+      },
+    }).then(
+      async (result) =>
+        await Task.update(
+          { TaskJSON: JSON.stringify(taskarr[i]) },
+          { where: { task_id: taskarr[i].task_id } }
+        )
+    );
+  }
 
   return res.status(201).json({
     success: "algorithm has completed execution",
-    projectJSONable: Algo.runGreedyAlgorithm(projectobj, 3).toJSONable(),
+    projectJSONable: finalAssignments,
   });
 };
 
